544c44029027c1caf70b5ec9482d3744d8d6c730
ebinmoolan@gmail.com <bennye@tcd.ie>
2019-02-26 13:52:23 +0000

    Fixed module export

--- src/data.js
+++ src/data.js
@@ -1,8 +1,10 @@
 function insert() {
-    var instance = new UserModel({userid: 'john',years: []});
+    var instance = new UserModel({ userid: 'john', years: [] });
 
     instance.save(function (err) {
         if (err) return null;
 
     });
 }
+
+module.exports.insert = insert;
